bcfae04c8e813d71123aea2a190f599f12ea5d06
ePoklad <elizaveta.korovnikova@gmail.com>
2024-10-24 10:59:28 +0300

    final

--- js/script.js
+++ js/script.js
@@ -61,5 +61,63 @@
     }
   });
 
+  // scroll
+  document.querySelectorAll('.js-scroll').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const href = this.getAttribute('href').substring(1);
+      const scrollTarget = document.getElementById(href);
+      const elementPosition = scrollTarget.getBoundingClientRect().top;
+
+      window.scrollBy({
+        top: elementPosition,
+        behavior: 'smooth'
+      });
+    });
+  });
+
+
+
+  const MOBILE_WIDTH = 1024;
+
+  function getWindowWidth() {
+    return Math.max(
+      document.body.scrollWidth,
+      document.documentElement.scrollWidth,
+      document.body.offsetWidth,
+      document.documentElement.offsetWidth,
+      document.body.clientWidth,
+      document.documentElement.clientWidth
+    );
+  }
+
+  function scrollToContent(link, isMobile) {
+    if (isMobile && getWindowWidth() > MOBILE_WIDTH) {
+      return;
+    }
+
+    const href = link.getAttribute('href').substring(1);
+    const scrollTarget = document.getElementById(href);
+    const elementPosition = scrollTarget.getBoundingClientRect().top;
+
+    window.scrollBy({
+      top: elementPosition,
+      behavior: 'smooth'
+    });
+  }
+
+  document.querySelectorAll('.js-scroll-link').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      scrollToContent(this, true);
+    });
+  });
+
+
+
+
+
 
 
